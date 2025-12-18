334c9843e0815dd498d8dcd60b67fd17045e96ad
Abhinash Yadav <yadavawenass@gmail.com>
2025-12-18 17:15:27 +0545

    Update note.txt with additional information on NodeJS and API concepts

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 const express = require("express")
 const app = express()
 
-
-
-app.listen(3000, function(){ //3000 is the port number and function() is callback function
-    console.log("server has started at port 3000");
+app.get("/", function(req,res){ 
+    res.json({   //json is the standard file for data transferring
+        name : "home page"
+    })
 })
+
+app.get("/about",(req,res)=>{
+    res.json ({
+        address: "about page address",
+        age: 20,
+        name : "abhinash"
+    })
+})
+
+app.get("/greet",(req,res)=>{
+    res.json({
+    greet: "Namaste World!!"
+    })
+})
+
+
+app.listen(3000, function(){ //3000 is the port number and function() is callback function and listen is the method
+    console.log("server has started at port 3000");
+}) 
